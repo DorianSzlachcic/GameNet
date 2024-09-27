851fe597bd52ddc75222e4137a91270dc1282e5c
DorianSzlachcic <dorianszlachcic@gmail.com>
2024-09-27 12:28:14 +0200

    SMTP config

--- GameNet/settings.py
+++ GameNet/settings.py
@@ -155,11 +155,13 @@ LOGOUT_REDIRECT_URL = '/'
 
 #Confirmation email
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 #Media
 
